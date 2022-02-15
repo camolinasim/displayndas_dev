6cbf69fbe2cc23f0ffa5b60ff09c1c1b52a6d6ba
Cristian Molina <cmutep@gmail.com>
2022-02-15 10:57:10 -0700

    Delete Docket works -- Delete works too

---
+++ deleteDocket.php
@@ -0,0 +1,57 @@
+<?php
+print_r($_GET);
+$redirect=  'Location: reportexample.php' ;
+session_start();
+
+if (($_SESSION['user']==null) && ($_SERVER['HTTP_HOST'] != 'localhost'))
+{
+  echo '<script type="text/javascript">parent.location.reload();</script>';
+  header( $redirect);
+  echo 'This website requires javascript enabled in order to work properly.';
+  exit();
+}
+
+// $scdocs_admins_access = $_SESSION["ADMIN_SCDOCS_DISPLAY"];
+//
+// if (!$scdocs_admins_access)
+// {
+//   exit();
+// }
+  $File = "ORSPSRVAPP02/webroot2/NDAs_dev/".($_GET["Docket"]);
+
+  //echo(__FILE__.":".__LINE__.' $File = '.$File.'<br>' );
+
+if (!is_file($File))
+{
+  $mssqldb_conn = new PDO("sqlsrv:server=orspsrvapp02.utep.edu;Database=displayndas_dev","orspbt","3T3p*r3N1w");
+  if (!$mssqldb_conn)
+  {
+    echo "Unable to connect to the MSSQL server";
+    exit(0);
+  }
+  $q = "DELETE FROM [displayndas_dev].[dbo].[ndas] WHERE Docket='".rawurldecode($_GET["Docket"])."'";
+  $result2 = $mssqldb_conn->query($q);
+  if (!$result2)
+  {
+    echo "query = $q<br><br>";
+    echo(__FILE__.":".__LINE__.' Error: ');
+    print_r($mssqldb_conn->errorInfo());
+    die();
+  }
+  echo "Docket '"."NDAs_dev/".rawurldecode($_GET["Docket"])."' has been deleted.";
+  exit();
+}
+if (is_file($File) && unlink($File)) {
+  echo "The file: $File has been deleted.";
+
+} else if (rmdir($File)) {
+
+  //echo "The directory $File has been deleted.<br>";
+  $File = str_replace('\TEST','',$File);
+  rmdir($File);
+  //echo "The directory $File has been deleted.<br>";
+  $File = str_replace('\Sub-Awards','',$File);
+  rmdir($File);
+  echo "The directory: $File has been deleted.<br>";
+}
+?>
--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if ($row2['NumberOfFiles'] != $NumberOfFiles)
 {
  $d = dir("\\\ORSPSRVAPP02\\webroot2\\NDAs_dev");
 
- // Delete all the records in table ndafiles..
+ // Delete all the records in table ndafiles...
 
  $q = "DELETE FROM [displayndas_dev].[dbo].[ndafiles];";
  $result = $mssqldb_conn->query($q);
@@ -493,7 +493,7 @@ if (!$mssqlresults)
                      // {
                      echo '<td>';
                        //echo <form action="DeleteFile.php" method="POST"  <input type="submit" value="Delete Docket!"></form>;
-                       echo '<form action="DeleteDocket.php?Docket=', $row['Docket'], '" method="post"> <input type="submit" value="Delete Docket!">';
+                       echo '<form action="deleteDocket.php?Docket=', rawurlencode($row['Docket']), '" method="post"> <input type="submit" value="Delete Docket!">';
                        //echo '<button class="btn btn-danger" value="DeleteFile.php?file=', $row['Docket'], '"> Delete Docket</button>';
 
                        echo '&nbsp;&nbsp;<button class="btn btn-primary" type="button" data-toggle="modal" data-target="#uploadDoc" value="https://orspweb2.utep.edu/displayndas_dev/UploadDocument.php?docket=' . $row['Docket'] . '">Upload File</button>
