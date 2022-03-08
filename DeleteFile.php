c95c61e0bcc18a2e09c53040c0673b2aef2e8992
Cristian Molina <cmutep@gmail.com>
2022-03-08 11:31:02 -0700

    (lineas 20-25)

--- DeleteFile.php
+++ DeleteFile.php
@@ -17,9 +17,15 @@ if (($_SESSION['user']==null) && ($_SERVER['HTTP_HOST'] != 'localhost'))
 // {
 //   exit();
 // }
-$File = "ORSPSRVAPP02/webroot2/NDAs_dev/".($_GET["file"]);
-
+$File = "\\inetpub\\webroot2\\NDAs_dev\\".($_GET["file"]); //obtains the path of the file with a "%2F" between docket and nda
+                                                           //e.g. \\inetpub\\webroot2\\NDAs_dev\\2020-0022%2Ftest.pdf
+                                                           //%2F comes from html turning the "/" char into ASCII
 //echo(__FILE__.":".__LINE__.' $File = '.$File.'<br>' );
+$path = str_replace("%2F","\\",$File); //replaces the "%2F" to "\\" to
+unlink($path);
+
+//ANYTHING UNDER THIS LINE IS USELESS NOW
+
 
 if (!is_file($File))
 {
@@ -39,6 +45,7 @@ if (!is_file($File))
     die();
   }
   echo "Docket '"."NDAs_dev/".rawurldecode($_GET["file"])."' has been deleted.";
+  header("Location: https://orspweb2.utep.edu/displayndas_dev/index.php"); //remove ths after adding ajax
   exit();
 }
 if (is_file($File) && unlink($File)) {
